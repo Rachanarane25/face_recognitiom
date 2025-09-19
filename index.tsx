ba685b7570215e7844323ec516d5981145c1abf5
Rachanarane25 <rachanarane25@gmail.com>
2025-09-20 00:42:17 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,10 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './style.css';
-
+import App from './App';
+import '../style.css'; // Adjust path depending on where style.css is located
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+if (!rootElement) throw new Error("Could not find root element");
 
 const root = ReactDOM.createRoot(rootElement);
 root.render(
